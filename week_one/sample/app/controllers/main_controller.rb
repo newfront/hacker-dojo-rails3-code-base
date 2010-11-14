d7ee9bf79c59fec64b9f2f6714632a36ea57bb41
Scott Haines <scott@newfrontcreative.com>
2010-11-14 09:17:02 -0800

    Simple Test and Lesson1 Walkthrough

---
+++ week_one/sample/app/controllers/main_controller.rb
@@ -0,0 +1,8 @@
+class MainController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+end
---
+++ week_one/sample/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample::Application.config.secret_token = '111bfa2ad123f133f3d13f70e948b0d1b570a9f327b3227b0f5bf4d470aaced33762232736781cf49e5a73c0c0535639f55e9538e4f4484902ba261535e7f10f'
